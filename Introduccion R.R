15b3559ec0b471457dbff0852433ae84c7adf349
gcasanovaelias <gcasanovaelias@gmail.com>
2022-01-06 19:43:05 -0300

    Intro

---
+++ Introduccion R.R
@@ -0,0 +1,9 @@
+# R es un lenguaje de programación basado en el objetos. 
+#* No sólo se pueden crear objetos si no que estos pueden formar parte de las operaciones (dentro de ellos o entre distintos objetos)
+
+# Clases de objetos ----
+#* Existen 4 tipos de objetos que son manejables en R 
+
+char <- c("Estadistica", "RRNN", "UCHILE") #Caracter
+
+num <- c(1,2,3) #Numérico
